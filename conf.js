88019ac7af0573838b01e8eba4e80b52999d1a1f
raghu mn <raghu.m@testyantra.com>
2020-02-23 21:10:20 +0530

    test

---
+++ conf.js
@@ -0,0 +1,47 @@
+// An example configuration file.
+var AllureReporter = require('jasmine-allure-reporter');
+
+exports.config = {
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine2',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['./example/example_spec.js'],
+
+  // Options to be passed to Jasmine.
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(new AllureReporter());
+    jasmine.getEnv().afterEach(function(done){
+      browser.takeScreenshot().then(function (png) {
+        allure.createAttachment('Screenshot', function () {
+          return new Buffer(png, 'base64')
+        }, 'image/png')();
+        done();
+      })
+    });
+
+  },
+
+  onComplete: function() {
+
+  },
+  jasmineNodeOpts: {
+    onComplete: null,
+    // If true, display spec names.
+    isVerbose: false,
+    // If true, print colors to the terminal.
+    showColors: true,
+    // If true, include stack traces in failures.
+    includeStackTrace: true,
+    // Default time to wait in ms before a test fails.
+    defaultTimeoutInterval: 120000
+  }
+};
